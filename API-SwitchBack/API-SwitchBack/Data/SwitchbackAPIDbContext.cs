4e50654bc9381b03ac218fee7c0ed9f4093b8042
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-04-15 13:44:49 -0700

    commits

---
+++ API-SwitchBack/API-SwitchBack/Data/SwitchbackAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_SwitchBack.Data
+{
+    public class SwitchbackAPIDbContext
+    {
+        public DbSet<SwitchbackAPIDbContext> APIController { get; set; }
+
+    }
+}
--- API-SwitchBack/API-SwitchBack/Startup.cs
+++ API-SwitchBack/API-SwitchBack/Startup.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API_SwitchBack.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace API_SwitchBack
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddDbContext<SwitchbackAPIDbContext>(options =>
+            options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +39,9 @@ namespace API_SwitchBack
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMvc();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+
         }
     }
 }
