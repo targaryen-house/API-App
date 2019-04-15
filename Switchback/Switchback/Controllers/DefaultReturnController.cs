15a5d411eca7c23e6e71625a3c2079d3cb4c91ac
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-15 11:50:42 -0700

    DefaultReturn complete

---
+++ Switchback/Switchback/Controllers/DefaultReturnController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Switchback.Models;
+using Switchback.Models.Interface;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System;
+using Microsoft.AspNetCore.Http;
+
+
+namespace Switchback.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultReturnController : ControllerBase
+    {
+        private readonly IDefaultReturn _context;
+        public DefaultReturnController(IDefaultReturn context){
+            _context = context;
+        }
+
+        // GET: api/DefaultReturn/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<ActionResult<DefaultReturn>> Get(int id)
+        {
+            DefaultReturn defaultReturn = await _context.GetDefaultReturn(id);
+            if (id < 1){
+                return NotFound();
+            }
+            return Ok(defaultReturn.Message);
+        }
+    }
+}
