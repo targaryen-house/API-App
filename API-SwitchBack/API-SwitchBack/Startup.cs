40d4676fc3c8f3d016ad3acfef2d5c4aaff8e0d7
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-15 17:26:01 -0700

    Minor changes to push database to local.

--- API-SwitchBack/API-SwitchBack/Startup.cs
+++ API-SwitchBack/API-SwitchBack/Startup.cs
@@ -37,6 +37,9 @@ namespace API_SwitchBack
 
             services.AddDbContext<SwitchbackAPIDbContext>(options =>
             options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            //DefaultConnection
+            //ProdutionConnection
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
